5d85d47ec49868645f7080f7a65ebfa47802332b
Andres  Garzon <andresgarzon@andress-imac.home>
2024-08-20 19:08:39 -0400

    card styles and render

--- load-more-items-app/src/App.js
+++ load-more-items-app/src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import LoadMoreItems from "./components/item-loader";
-
+import "./components/item-loader/index.css";
 function App() {
   return (
     <div className="App">
--- load-more-items-app/src/components/item-loader/index.jsx
+++ load-more-items-app/src/components/item-loader/index.jsx
@@ -3,22 +3,48 @@ import { useEffect, useState } from "react";
 export default function LoadMoreItems({ url }) {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [moreItem, setMoreItems] = useState(0);
   async function fetchDataItems() {
     try {
       setLoading(true);
 
-      const response = await fetch(`${url}?limit=20&skip=0`);
+      const response = await fetch(`${url}?limit=20&skip=${moreItem}`);
       const dataItem = await response.json();
       console.log(dataItem);
-      setProducts(dataItem);
+      setProducts(dataItem.products);
       setLoading(false);
     } catch (error) {
       console.log(error);
     }
   }
+
+  function handleMoreItems() {
+    setMoreItems(moreItem + 20);
+  }
+
   useEffect(() => {
     fetchDataItems();
-  }, []);
-  if (loading) return <>Loading... Please wait!</>;
-  return <>Hello World</>;
+  }, [moreItem]);
+  if (loading) return <h1>Loading... Please wait!</h1>;
+  return (
+    <div>
+      <div className="card-wrapper">
+        {products && products.length > 0
+          ? products.map((item) => (
+              <div className="card-container">
+                <img
+                  src={item.images}
+                  alt={item.title}
+                  style={{ width: "300px" }}
+                />
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+                <p className="price-tag">$ {item.price}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <button onClick={handleMoreItems}>Load More Items</button>
+    </div>
+  );
 }
